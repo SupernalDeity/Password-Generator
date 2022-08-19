9f29d3649d1b5bd939b959f9f25b1d8f1faa1963
Anthony <perezant121@gmail.com>
2022-08-18 21:22:57 -0400

    Added promts and confirms to get password requirements

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword()
-{ var lower = 'abcdefghijklmnopqrstuvwxyz';
+function generatePassword() { 
+  var lower = 'abcdefghijklmnopqrstuvwxyz';
   var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var special = '#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
   var numeric = '0123456789';
-}
 
+  var passwordLength = prompt("How many characters long would you like your password to be? Choose a number between 8 and 128");
+
+  if (!passwordLength) {
+    alert('Please enter a number');
+  }
+
+  else if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a number between 8 and 128");
+  }
+
+  else {
+    var hasLower = confirm('Do you want lowercase letters?');
+    var hasUpper = confirm('Do you want uppercase letters?');
+    var hasSpecial = confirm('Do you want special characters?');
+    var hasNumeric = confirm('Do you want numbers?');
+  }
+
+
+
+  // if (hasLower) {
+  // possible += lower;
+  // }
+
+}
 
 
 
