620b676eb62e6943daa49e1a5f3e9229f34a59c3
Anthony <perezant121@gmail.com>
2022-08-18 20:40:25 -0400

    added initial variables for password

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  var lower = 'abcdefghijklmnopqrstuvwxyz';
+  var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var special = '#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
+  var numeric = '0123456789';
 }
+
+
+
+
+
+
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
